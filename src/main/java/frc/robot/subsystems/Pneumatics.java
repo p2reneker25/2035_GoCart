16ecd07677c553ec4c451ace270da5ad58607188
Roy <89213640+p2reneker25@users.noreply.github.com>
2022-05-05 13:59:53 -0700

    started gearshift system

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -12,10 +12,15 @@ public class Pneumatics extends SubsystemBase{
     private Solenoid horn;
     private DoubleSolenoid left_shock;
     private DoubleSolenoid right_shock;
+    private DoubleSolenoid left_gearbox;
+    private DoubleSolenoid right_gearbox;
+    
     public Pneumatics(){
         horn = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.Pneumatics.PNEUMATICS_HORN);
         left_shock = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Pneumatics.PNEUMATICS_LEFT_SHOCK_UP, Constants.Pneumatics.PNEUMATICS_LEFT_SHOCK_DN);
         right_shock = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Pneumatics.PNEUMATICS_RIGHT_SHOCK_UP, Constants.Pneumatics.PNEUMATICS_RIGHT_SHOCK_DN);
+        left_gearbox = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Pneumatics.PNEUMATICS_LEFT_GEARBOX_HI, Constants.Pneumatics.PNEUMATICS_LEFT_GEARBOX_LO);
+        right_gearbox = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Pneumatics.PNEUMATICS_RIGHT_GEARBOX_HI, Constants.Pneumatics.PNEUMATICS_RIGHT_GEARBOX_LO);
     }
     public void leftShockUp(){
         left_shock.set(Value.kForward);
@@ -37,8 +42,34 @@ public class Pneumatics extends SubsystemBase{
     }
     public void disableLeftShock(){
         left_shock.set(Value.kOff);
-    }public void disableRightShock(){
+    }
+    public void disableRightShock(){
         right_shock.set(Value.kOff);
     }
- 
+    public void setGear(boolean gearbox, boolean pos){
+        if(gearbox){ //left gearbox
+            if(pos){
+                left_gearbox.set(Value.kForward);
+            }
+            else{
+                left_gearbox.set(Value.kReverse);
+            }
+        }
+        else{
+            if(pos){
+                right_gearbox.set(Value.kForward);
+            }
+            else{
+                right_gearbox.set(Value.kReverse);
+            }
+        }
+    }
+    public void disableLeftGearshift(){
+        left_gearbox.set(Value.kOff);
+    }
+    public void disableRightGearshift(){
+        right_gearbox.set(Value.kOff);
+    }
+
+
 }
